43bd1167e56820c9e419ba30b797fd40f97afc5c
sᴀᴅᴇᴡ ᴊᴀʏᴀsᴇᴋᴀʀᴀ <84616471+Sadew451@users.noreply.github.com>
2021-11-08 15:00:35 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -6,9 +6,9 @@ from helpers.filters import other_filters2
 
 @Client.on_message(other_filters2)
 async def start(_, message: Message):
-    await message.reply_sticker_id(sticker_id = "CAACAgUAAxkBAAEBN9FhhMQq99NL0eY70r47LmPghi9fsQAC2QUAAss5KFT5r0lsTegDZiIE")
-         ** @SDbotsz. 🔥",
+    await message.reply_text(
         f"""I am an Telegram Groups Music Player🎶, I let you play music in your group's voice chat.
+
 The commands I currently support are:
 `/play` - 🎶 Play the replied audio file or YouTube video 
 `/pause` - ▶️ Pause the audio stream 
