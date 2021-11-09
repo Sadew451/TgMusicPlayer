f213cea94f2cceb24a61e3e4ee8fe0b904c17310
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-11-09 18:10:50 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -5,9 +5,17 @@ from helpers.filters import other_filters2
 
 STICKER = "CAACAgQAAx0CQ2C8OgACsqNhiisoWUQROohUrpaGzDsHsot3dQACVxYAAtqjlSznBlAxygdMwyIE"
 
-TEXT = "👋 Hey [{}](tg://user?id={}), I am an Telegram Groups Music Player, I let you play music in your group's voice chat.\n\n**Commands** [Here](telegra.ph/A-Simple-Group-Music-player-bot-by-SDBotsz-11-09-2)\n\nJoin @SDBotsz. 🔥"
+@Client.on_message(other_filters2)
+async def start(_, message: Message):
+    await message.reply_sticker(STICKER)
+    await message.reply_text(
+        f"""👋 Hey [{}](tg://user?id={}), I am an Telegram Groups Music Player, I let you play music in your group's voice chat.
 
-BUTTONS = InlineKeyboardMarkup(
+**Commands** [Here](telegra.ph/A-Simple-Group-Music-player-bot-by-SDBotsz-11-09-2)
+
+Join @SDBotsz. 🔥
+        """,
+        reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
@@ -19,14 +27,5 @@ BUTTONS = InlineKeyboardMarkup(
                 ]
             ]
         )
-    )
-
-@Client.on_message(other_filters2)
-async def start(_, message: Message):
-    await message.reply_sticker(STICKER)
-    await message.reply_text(
-        text=TEXT,
-        reply_markup=BUTTONS,
+    ),
         disable_web_page_preview=True
-    )
-        
