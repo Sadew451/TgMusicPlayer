0e69c1a37ec6a01c31e8009462ffcf398acd2994
Sᴀᴅᴇᴡ Jᴀʏᴀsᴇᴋᴀʀᴀ <84616471+Sadew451@users.noreply.github.com>
2021-11-09 13:33:55 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -3,15 +3,15 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 from helpers.filters import other_filters2
 
-STICKER = "CAACAgUAAxkBAAEBSiphiQvgoSco_PZ69hZOb6LmHi7y5wACrgUAAqcESVQjjAq485SvXyIE"
+STICKER = "CAACAgQAAx0CQ2C8OgACsqNhiisoWUQROohUrpaGzDsHsot3dQACVxYAAtqjlSznBlAxygdMwyIE"
 
 @Client.on_message(other_filters2)
 async def start(_, message: Message):
     await message.reply_sticker(STICKER)
     await message.reply_text(
-        f"""👋 Hey **{message.from_user.first_name }**, I am an Telegram Groups Music Player, I let you play music in your group's voice chat.
+        f"""👋 Hey `{message.from_user.first_name }`, I am an Telegram Groups Music Player, I let you play music in your group's voice chat.
 
-Commands [Here](telegra.ph/A-Simple-Group-Music-player-bot-by-SDBotsz-11-09-2)
+**Commands** [Here](telegra.ph/A-Simple-Group-Music-player-bot-by-SDBotsz-11-09-2)
 
 Join @SDBotsz. 🔥
         """,
