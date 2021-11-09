7c80ed0fc7a1a2c2f959ea964b5c61c0c91cf0d7
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-11-09 18:25:18 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -5,25 +5,30 @@ from helpers.filters import other_filters2
 
 STICKER = "CAACAgQAAx0CQ2C8OgACsqNhiisoWUQROohUrpaGzDsHsot3dQACVxYAAtqjlSznBlAxygdMwyIE"
 
-@Client.on_message(other_filters2)
-async def start(_, message: Message):
-    await message.reply_sticker(STICKER)
-    await message.reply_text(
-        f"""👋 Hey [{}](tg://user?id={}), I am an Telegram Groups Music Player, I let you play music in your group's voice chat.
+
+START_TEXT = """
+👋 Hey {} I am an Telegram Groups Music Player, I let you play music in your group's voice chat.
+
 **Commands** [Here](telegra.ph/A-Simple-Group-Music-player-bot-by-SDBotsz-11-09-2)
+
 Join @SDBotsz. 🔥
-""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "Channel 🙋‍♀️", url="https://t.me/SDBOTs_inifinity"
-                    ),
-                    InlineKeyboardButton(
-                        "Developer 👩‍💻", url="https://t.me/Itz_Sadew"
-                    )
-                ]
-            ]
-        )
-    ),
-        disable_web_page_preview=True
+"""
+
+START_BUTTON = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Channel', url="https://t.me/SDBOTs_inifinity"),
+        InlineKeyboardButton('Group', url='https://t.me/SDBOTz')
+        ]]
+  
+)
+
+@Client.on_message(filters.private & filters.command(["start"]))
+async def start(bot, update):
+    await update.reply_sticker(STICKER)
+    await update.reply_text(
+        text=START_TEXT.format(update.from_user.mention),
+        reply_markup=START_BUTTON,
+        disable_web_page_preview=True,
+        quote=True
+    )
+
