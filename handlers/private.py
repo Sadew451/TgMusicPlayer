bfb607a01ceaff2e5b15def0b1b8ec261f8ce5ab
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-11-09 18:18:26 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -10,9 +10,7 @@ async def start(_, message: Message):
     await message.reply_sticker(STICKER)
     await message.reply_text(
         f"""👋 Hey [{}](tg://user?id={}), I am an Telegram Groups Music Player, I let you play music in your group's voice chat.
-
 **Commands** [Here](telegra.ph/A-Simple-Group-Music-player-bot-by-SDBotsz-11-09-2)
-
 Join @SDBotsz. 🔥
 """,
         reply_markup=InlineKeyboardMarkup(
